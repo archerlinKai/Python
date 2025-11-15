7db3fe7e0d9e52b3423530037708c67788351f98
archerlinKai <archerlin.kai@gmail.com>
2025-11-15 15:18:48 +0800

    優化螺旋狀圖形繪製邏輯，統一速度設定並調整蓋章位置；刪除不必要的臨時檔案

--- class4/hw02.py
+++ class4/hw02.py
@@ -1,35 +1,43 @@
 # 螺旋狀圖形
 import turtle as t  # 匯入模組turtle並命名為t
 
+t.shape("circle")
+t.color("red")
+t.penup()  # 提起畫筆,蓋章時不會有線
 for i in range(15):
-
+    t.stamp()
+    t.speed(0)
     t.shape("circle")
     t.forward(10)
-    t.stamp()
     t.right(8)
-for i in range(15):
 
+for i in range(15):
+    t.stamp()
+    t.speed(0)
     t.forward(20)
-    t.stamp()
-    t.right(10)
-for i in range(15):
+    t.right(8)
 
+for i in range(15):
+    t.stamp()
+    t.speed(0)
     t.forward(30)
-    t.stamp()
-    t.right(12)
-for i in range(15):
+    t.right(8)
 
+for i in range(15):
+    t.stamp()
+    t.speed(0)
     t.forward(40)
-    t.stamp()
-    t.right(14)
-for i in range(15):
+    t.right(8)
 
+for i in range(15):
+    t.stamp()
+    t.speed(0)
     t.forward(50)
-    t.stamp()
-    t.right(16)
-for i in range(15):
+    t.right(8)
 
-    t.forward(60)
+for i in range(15):
     t.stamp()
-    t.right(18)
+    t.speed(0)
+    t.forward(60)
+    t.right(8)
 t.done()  # 不要關閉視窗
