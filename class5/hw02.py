4c9e55441005aa5311198059b73ef8c69fef58a5
archerlinKai <archerlin.kai@gmail.com>
2025-11-22 15:04:42 +0800

    調整hw02.py中的秒針繪製邏輯，移除不必要的暫停與回到原點動作；在hw05.py中統一畫筆變數名稱並擴展圖形繪製功能；新增prj01.py檔案

--- class5/hw02.py
+++ class5/hw02.py
@@ -7,9 +7,10 @@ tr.forward(100)  # 向前移動100單位
 tr.hideturtle()  # 隱藏畫筆箭頭
 
 for i in range(60):  # 重複60次
+
+    tr.forward(100)  # 向前移動100單位
+    tr.back(100)
+    t.sleep(1)  # 暫停1秒鐘
     tr.clear()  # 清除畫面
     tr.right(6)  # 向右轉6度
-    tr.stamp()  # 蓋印章
-    t.sleep(1)  # 暫停1秒鐘
-    tr.home()  # 回到原點
 tr.done()  # 不要關閉視窗
--- class5/hw05.py
+++ class5/hw05.py
@@ -1,11 +1,41 @@
-import turtle
+import turtle as t
 
-t = turtle.Turtle()
+t.speed(0)
 
-for i in range(12):
+for i in range(4):
     t.forward(80)
     t.stamp()
     t.home()
     t.right(30)
+for i in range(4):
+    t.forward(80)
+    t.stamp()
+    t.home()
+    t.right(55)
+for i in range(4):
+    t.forward()
+    t.stamp()
+    t.home()
+    t.right(75)
+for i in range(4):
+    t.forward()
+    t.stamp()
+    t.home()
+    t.right(90)
+for i in range(4):
+    t.forward()
+    t.stamp()
+    t.home()
+    t.right(105)
+for i in range(4):
+    t.forward()
+    t.stamp()
+    t.home()
+    t.right(125)
+for i in range(4):
+    t.forward()
+    t.stamp()
+    t.home()
+    t.right(150)
 
-turtle.done()
+    t.done()
